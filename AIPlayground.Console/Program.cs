35b45ad3a905fb8c574967117b61584eef1a3d57
Alexander Klassanov <klassanov@live.com>
2026-04-27 13:22:54 +0300

    Update chat to use stateful history and scientist prompt

--- AIPlayground.Console/Program.cs
+++ AIPlayground.Console/Program.cs
@@ -18,7 +18,6 @@ var configuration = new ConfigurationBuilder()
 var settings = new SemanticKernelSettings();
 configuration.GetSection("SemanticKernel").Bind(settings);
 
-
 var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(
     deploymentName: settings.AzureOpenAI.DeploymentName,
@@ -27,50 +26,67 @@ builder.AddAzureOpenAIChatCompletion(
 
 var kernel = builder.Build();
 
-
 //This works regardless of the model
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
 
 //Specific to Open AI. Each model has a different class for settings. WIll not work with other models.
 var promptExecutionSettings = new OpenAIPromptExecutionSettings()
 {
     //System prompt
-    ChatSystemPrompt = "You are a helpful assistant that provides concise and accurate answers to user questions. Always answer as a pirate.",
-    
+    ChatSystemPrompt = "You are a helpful assistant that provides concise and accurate answers to user questions. Answer as a scientist.",
+
     //Pretty different response eeach time
     Temperature = 0.9,
 
-    //Max token in the response
+    //Max token in the response => limit the cost
     MaxTokens = 100
 };
 
+var chatHistory = new ChatHistory();
 
 while (true)
 {
     Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Enter your prompt here. Press 'q' or 'Enter' to quit");
-
     Console.ForegroundColor = ConsoleColor.DarkYellow;
     var userPrompt = Console.ReadLine();
-    if(string.IsNullOrEmpty(userPrompt) || userPrompt.Equals("q", StringComparison.OrdinalIgnoreCase)) 
+
+    if (string.IsNullOrEmpty(userPrompt) || userPrompt.Equals("q", StringComparison.OrdinalIgnoreCase))
     {
         break;
     }
 
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    var chatResponse = await chatCompletionService.GetChatMessageContentAsync(userPrompt, promptExecutionSettings);
-    Console.WriteLine(chatResponse.Content);
+    chatHistory.AddUserMessage(userPrompt);
 
-    if(chatResponse.InnerContent is ChatCompletion)
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    
+    //Stateliess
+    //var chatResponse = await chatCompletionService.GetChatMessageContentAsync(userPrompt, promptExecutionSettings);
+    
+    //Send chat history, so there is no loss of context.
+    var chatResponse = await chatCompletionService.GetChatMessageContentAsync(chatHistory, promptExecutionSettings);
+
+    if (chatResponse !=null)
     {
-        Console.ForegroundColor = ConsoleColor.DarkCyan;
-        Console.WriteLine("Detailed info:");
-        var details = chatResponse.InnerContent as ChatCompletion;
-        Console.WriteLine($"ModelId: {chatResponse.ModelId}");
-        Console.WriteLine($"Output token count: {details!.Usage.OutputTokenCount}");
-        Console.WriteLine($"Input token count: {details!.Usage.InputTokenCount}");
-        Console.WriteLine($"Total token count: {details!.Usage.TotalTokenCount}");
+        chatHistory.AddAssistantMessage(chatResponse.Content ?? string.Empty);
+        Console.WriteLine(chatResponse.Content);
+
+        if (chatResponse.InnerContent is ChatCompletion)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Detailed info:");
+            var details = chatResponse.InnerContent as ChatCompletion;
+            Console.WriteLine($"ModelId: {chatResponse.ModelId}");
+            Console.WriteLine($"Output token count: {details!.Usage.OutputTokenCount}");
+            Console.WriteLine($"Input token count: {details!.Usage.InputTokenCount}");
+            Console.WriteLine($"Total token count: {details!.Usage.TotalTokenCount}");
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("No response from the model."); 
+        Console.ForegroundColor = ConsoleColor.White;
     }
 
     Console.WriteLine();
