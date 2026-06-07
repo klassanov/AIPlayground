9d502ffa8bd332274611444ac56828ec3b00cba3
Alexander Klassanov <klassanov@live.com>
2026-06-07 14:46:16 +0300

    Added a new project for image analysis

---
+++ AIMultimodal.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var service = new AzureOpenAIChatCompletionService(
+    deploymentName: configuration["AzureOpenAI:DeploymentName"],
+    apiKey: configuration["AzureOpenAI:ApiKey"],
+    endpoint: configuration["AzureOpenAI:Endpoint"],
+    modelId: configuration["AzureOpenAI:ModelId"]
+    );
