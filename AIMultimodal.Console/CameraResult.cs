7e4d8468a8122f51cf98b7efe167d8d003b21194
Alexander Klassanov <klassanov@live.com>
2026-06-17 18:28:46 +0300

    Add structured output for traffic analysis results

---
+++ AIMultimodal.Console/CameraResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIMultimodal.Console
+{
+    //2 ways of generating strucutred output in C# and .NET
+    //The LLM shall support structured outputs
+    //1. Use a class (current case)
+    //2. Use a json schema
+
+    internal record CameraResult(bool IsBroken, TrafficCongestionLevel CongestionLevel, string Analysis);
+
+    public enum TrafficCongestionLevel
+    {
+        Light,
+        Moderate,
+        High,
+        Unknown
+    }
+
+}
--- AIMultimodal.Console/Program.cs
+++ AIMultimodal.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using AIMultimodal.Console;
+using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
@@ -18,7 +21,23 @@ var service = new AzureOpenAIChatCompletionService(
     modelId: configuration["AzureOpenAI:ModelId"]
     );
 
+var executionSettings = new AzureOpenAIPromptExecutionSettings()
+{
+    ResponseFormat = typeof(CameraResult)
+};
+
 var imageFileNames = Directory.GetFiles("images");
+var deserializerOptions = new JsonSerializerOptions
+{
+    // Note:
+    // This is a collection initializer syntax, which doesn't reassign the property:
+    // This syntax is equivalent to deserializerOptions.Converters.Add(new JsonStringEnumConverter());
+    // We are not reassigning, but mutating, it adds the converter to the existing list of converters
+    
+    Converters = { new JsonStringEnumConverter() }
+};
+//This syntax is equivalent to deserializerOptions.Converters.Add(new JsonStringEnumConverter());
+//deserializerOptions.Converters.Add(new JsonStringEnumConverter());
 
 foreach (var imageFileName in imageFileNames)
 {
@@ -34,15 +53,17 @@ Medium congestion is when there is a lot of cars but they are not braking.
 Low traffic is when there are few cars on the road.
 In addition, attempt to determine if the image was taken with a malfunctioning camera by looking for distorted image or missing content");
 
-
-
     chatHistory.AddUserMessage(contentItems: [
         new ImageContent(imageBytes, "image/jpeg"),
         new TextContent("Analyze the image and determine the traffic congestion level. Also determine if camera is malfunctioning.")
     ]);
 
+    var response = await service.GetChatMessageContentAsync(
+       chatHistory: chatHistory,
+       executionSettings: executionSettings);
 
-    var response = await service.GetChatMessageContentAsync(chatHistory);
+    var cameraResult = JsonSerializer.Deserialize<CameraResult>(response.Content, deserializerOptions);
+
     Console.WriteLine($"Response: {response.Content}");
     Console.WriteLine(new string('-', 50));
 
